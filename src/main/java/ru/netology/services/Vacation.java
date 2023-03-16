58b07486ba3165b4aa72441f7d70a9629295fa8d
Vasily Ermolaev <vasily.erm@gmail.com>
2023-03-17 00:27:40 +0300

    exercise2

--- src/main/java/ru/netology/services/Vacation.java
+++ src/main/java/ru/netology/services/Vacation.java
@@ -2,18 +2,18 @@ package ru.netology.services;
 
 public class Vacation {
     public int calculate(int income, int expenses, int threshold) {
-        int counter = 0;
+        int count = 0;
         int money = 0;
 
-        for (int i = 0; i <= 12; i++) {
+        for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
                 money = money - expenses;
                 money = money / 3;
-                counter++;
+                count++;
             } else {
-                money = money + income + expenses;
+                money = money + income - expenses;
             }
         }
-        return counter;
+        return count;
     }
 }
--- src/test/java/ru/netology/services/VacationTest.java
+++ src/test/java/ru/netology/services/VacationTest.java
@@ -2,6 +2,8 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,26 @@ class VacationTest {
         int actual = service.calculate(income, expenses, threshold);
         int expected = 3;
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        Vacation service = new Vacation();
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/test3.csv")
+    public void testWithParams(int income, int expenses, int threshold, int expected) {
+        Vacation service = new Vacation();
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
